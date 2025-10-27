99ea92fadeeb030f45f1c6ee805e36bb2bd311c3
hemssy <hemsyeons@gmail.com>
2025-10-27 16:11:33 +0900

    [Chore] 디버깅 로그 제거하기

--- MyBookShelf/MyBookShelf/Network/BookService.swift
+++ MyBookShelf/MyBookShelf/Network/BookService.swift
@@ -16,24 +16,10 @@ class BookService {
         request.httpMethod = "GET"
         request.addValue("KakaoAK \(apiKey)", forHTTPHeaderField: "Authorization")
 
-
         URLSession.shared.dataTask(with: request) { data, response, error in
-            // [디버깅1] 에러 체크
             if let error = error {
-                print("네트워크 에러 발생:", error.localizedDescription)
                 completion(.failure(error))
                 return
-            }
-
-            // [디버깅2] HTTP 상태 코드 확인
-            if let httpResponse = response as? HTTPURLResponse {
-                print("응답 코드:", httpResponse.statusCode)
-            }
-
-            // [디버깅3] 실제 응답 데이터 확인 (문자열 형태)
-            if let data = data,
-               let jsonString = String(data: data, encoding: .utf8) {
-                print("응답 JSON 미리보기:\n\(jsonString.prefix(300))...\n")
             }
 
             guard let data = data else {
@@ -44,9 +30,7 @@ class BookService {
             do {
                 let decoded = try JSONDecoder().decode(BookSearchResponse.self, from: data)
                 completion(.success(decoded.documents))
-                print("디코딩 성공 — 책 개수:", decoded.documents.count)
             } catch {
-                print("디코딩 에러:", error)
                 completion(.failure(error))
             }
         }.resume()
